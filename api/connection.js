13aa77739eb1bd7c4f3a95fadb299bbb77af2402
Ankit Vishwakarma <ankit.coolzzzz@gmail.com>
2024-03-30 23:15:20 +0530

    bug fixed

--- api/connection.js
+++ api/connection.js
@@ -1,17 +1,14 @@
 const { Client } = require('pg');
 
 const client = new Client({
-	user: 'postgres',
-	password: 'root',
-	host: '::1',
+	user: 'viga_c0g9_user',
+	password: '5gaq899lsi0sbnBrySEOL8XyafzcS6dV',
+	host: 'dpg-co3i3ckf7o1s738hov50-a.oregon-postgres.render.com',
 	port: '5432',
-	database: 'postgres',
+	database: 'viga_c0g9',
 });
  
  
-    
-
-
  
 
 client.connect().then(()=>{
