fda1bc9be0f9bf2bf1e42f3009c49b298cea95bf
Ankit Vishwakarma <ankit.coolzzzz@gmail.com>
2024-03-30 23:47:07 +0530

    bug fixed

--- api/connection.js
+++ api/connection.js
@@ -1,21 +1,22 @@
 const { Client } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const client = new Client({
-	user: 'viga_c0g9_user',
-	password: '5gaq899lsi0sbnBrySEOL8XyafzcS6dV',
-	host: 'dpg-co3i3ckf7o1s738hov50-a.oregon-postgres.render.com',
-	port: '5432',
-	database: 'viga_c0g9',
+    user: "viga_c0g9_user",
+    password: "5gaq899lsi0sbnBrySEOL8XyafzcS6dV",
+    host: 'dpg-co3i3ckf7o1s738hov50-a.oregon-postgres.render.com',
+    port:5432,
+    database:"viga_c0g9",
+    ssl: true,  
 });
- 
- 
- 
 
-client.connect().then(()=>{
-    console.log('connected to postgreSQL DB');
-}).catch((err)=>{
-    console.error('error connecting failure:',err)
-})
+client.connect() 
+    .then(() => {
+        console.log('Connected to PostgreSQL DB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to PostgreSQL:', err);
+    }); 
 
-module.exports = client;
-
+module.exports = client; 
